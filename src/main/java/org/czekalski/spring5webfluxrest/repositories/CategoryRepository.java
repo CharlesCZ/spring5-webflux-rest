52e9b0386105d4cc0ba0298188063c06ec927b3d
CharlesCZ <karakankaczyn@gmail.com>
2019-05-09 00:01:07 +0200

    Initial commit

---
+++ src/main/java/org/czekalski/spring5webfluxrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package org.czekalski.spring5webfluxrest.repositories;
+
+import org.czekalski.spring5webfluxrest.domain.Category;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface CategoryRepository extends ReactiveMongoRepository<Category,String> {
+}
