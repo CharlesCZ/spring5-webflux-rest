c5d0f25348a1ffa9994b593182c903f3111c36b1
CharlesCZ <karakankaczyn@gmail.com>
2019-05-12 13:06:25 +0200

    bootstrap reactive

---
+++ src/main/java/org/czekalski/spring5webfluxrest/repositories/VendorRepository.java
@@ -0,0 +1,7 @@
+package org.czekalski.spring5webfluxrest.repositories;
+
+import org.czekalski.spring5webfluxrest.domain.Vendor;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface VendorRepository extends ReactiveMongoRepository<Vendor,String> {
+}
